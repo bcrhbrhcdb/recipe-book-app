92e6d12d0beadd1b8bdee116cabc83c8e2efadd7
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-01 12:57:41 -0500

    stuff :)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello!');
+let recipes = [];
+let previewOfRecipe = []
+const recipeArea = document.getElementById("recipe-area");
+const addRecipe = document.getElementById("add-reicipe");
+const editRecipe = document.getElementById("edit-recipe");
+const removeRecipe = document.getElementById("remove-recipe");
+const addRecipeInfo = document.getElementById("add-recipe-info");
+
+/* class RecipeFormat{
+    constructor(){
+        this.title = title;
+    }
+}
+*/
