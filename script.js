b0f01a9efbe59cf7f6c46c55293be39e6765bbfe
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-01 13:06:45 -0500

    js and more html...

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const addRecipe = document.getElementById("add-reicipe");
 const editRecipe = document.getElementById("edit-recipe");
 const removeRecipe = document.getElementById("remove-recipe");
 const addRecipeInfo = document.getElementById("add-recipe-info");
+const category = document.getElementById("category");
+// basic information
 
-/* class RecipeFormat{
-    constructor(){
-        this.title = title;
-    }
-}
-*/
+const recipeName = document.getElementById("recipe-name-input");
+const serves = document.getElementById("serves");
+const prepTime = document.getElementById("prep-time");
+const cookTime = document.getElementById("cook-time");
+
+// ingredients
+const ingredients = document.getElementById("ingredients")
